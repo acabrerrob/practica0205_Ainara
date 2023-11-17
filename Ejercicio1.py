8341fb98d0058761fe40a3ee0c95d8cb5b743a7b
acabrerrob <acabrerrob@educacion.navarra.es>
2023-11-17 16:54:30 +0100

    Update Ejercicio1.py

---
+++ Ejercicio1.py
@@ -0,0 +1,10 @@
+
+def saludo(nombre) :
+    """Función que se le pase una cadena <nombre> e imprima la frase ¡hola <nombre>!
+    
+    Parámetro = nombre: un nombre introducido por el usuario
+    Salida: Aparecerá en pantalla la frase ¡hola <nombre>!
+    """
+
+    print('¡hola', nombre, '!')
+    return
